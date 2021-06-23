8379a0a3fa36ca5038af40ee7811530167eea071
Zu <zu@netmek.biz>
2021-06-23 15:50:03 +1000

    social title

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -146,6 +146,7 @@ const Main = () => {
     } else {
         const contentData = useSelector(s=>s.sheets.content);
         const cta = useSelector(s=>s.sheets.global[0].cta);
+        const globalData = useSelector(s=>s.sheets.global[0]);
         const relatedItems = useSelector(s=>s.sheets.related);
         const [data, setData] = useState({});
 
@@ -261,7 +262,7 @@ const Main = () => {
                             <hr/>
                         </div>
 
-                        <SocialBar />
+                        <SocialBar title={globalData.shareTitle} url={globalData.url} />
                     </PaddedContainer>
 
                 </section>
